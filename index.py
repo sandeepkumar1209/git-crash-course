20209eb7020de7089a3788136296a647e965ce1e
sandeepkumar1209 <59911291+sandeepkumar1209@users.noreply.github.com>
2024-04-27 21:24:26 +0530

    adding feature in age variable in index.py

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
 print('Hello World!!!')
 name = 'Sandeep Panda'
-myage = 25
+final myage = 27
 address = 'Angul'
 print ('Name of the person is '+ name)
-print ('Age is '+ age)
+print ('Age is '+ myage)
 print ('Age is '+ address)
 
 print('Tomorrow I will work on this code')
